50751a2ee010ced266c25fad52e66bc133c59bed
Thayna Costa <thaynab170@gmail.com>
2025-07-28 17:41:39 -0400

    feat: componentizar o calendário semanal da página insights

--- src/app/components/pages/insights/insights.component.ts
+++ src/app/components/pages/insights/insights.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../shared/header/header.component';
+import { CalendarWeeklyComponent } from './calendar-weekly/calendar-weekly.component';
 
 @Component({
   selector: 'app-insights',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, CalendarWeeklyComponent],
   templateUrl: './insights.component.html',
   styleUrls: ['./insights.component.scss'],
 })
