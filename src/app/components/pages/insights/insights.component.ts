d064fb1afa1a7d214a15b1c76d5a098388b77623
Thayna Costa <thaynab170@gmail.com>
2025-07-28 17:26:03 -0400

    feat: adicionar header component na página insights

--- src/app/components/pages/insights/insights.component.ts
+++ src/app/components/pages/insights/insights.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../../shared/header/header.component';
 
 @Component({
   selector: 'app-insights',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './insights.component.html',
   styleUrls: ['./insights.component.scss'],
 })
