e5c93b7cd119ee00517ac9937dc3a136a461e937
Thayna Costa <thaynab170@gmail.com>
2025-07-28 17:41:39 -0400

    feat: componentizar o calendário semanal da página insights

---
+++ src/app/components/pages/insights/calendar-weekly/calendar-weekly.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-calendar-weekly',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './calendar-weekly.component.html',
+  styleUrls: ['./calendar-weekly.component.scss']
+})
+export class CalendarWeeklyComponent {
+  dias = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'];
+
+  eventos = [
+    ['', [
+      'Desenvolvimento Web Full Stack',
+      'Algoritmo e Estrutura de Dados I',
+      'Cálculo I'
+    ]],
+    ['', [
+      'Desenvolvimento Web Full Stack',
+      'Algoritmo e Estrutura de Dados I',
+      'Cálculo I'
+    ]],
+    ['', [
+      'Redes de Computadores'
+    ]],
+    ['', [
+      'Desenvolvimento Web Full Stack',
+      'Inteligência Artificial'
+    ]],
+    ['', [
+      'Desenvolvimento Web Full Stack',
+      'Banco de Dados'
+    ]]
+  ];
+}
