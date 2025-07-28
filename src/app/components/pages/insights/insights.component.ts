839ee9f111e5186e9fb4191a57d7549eefba9e16
Thayna Costa <thaynab170@gmail.com>
2025-07-28 16:54:25 -0400

    feat: criar arquivos para a página de insights

---
+++ src/app/components/pages/insights/insights.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-insights.component',
+  imports: [],
+  templateUrl: './insights.component.html',
+  styleUrl: './insights.component.scss'
+})
+export class InsightsComponent {
+
+}
