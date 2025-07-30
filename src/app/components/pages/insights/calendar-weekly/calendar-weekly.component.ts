e497f49780fa8b0eaec8860ff38a43e6e5f0f3f1
Thayna Costa <thaynab170@gmail.com>
2025-07-30 16:18:55 -0400

    refactor(style): mudar calendários e cards de insights para tornar mais responsivo e ajustado

--- src/app/components/pages/insights/calendar-weekly/calendar-weekly.component.ts
+++ src/app/components/pages/insights/calendar-weekly/calendar-weekly.component.ts
@@ -12,26 +12,12 @@ export class CalendarWeeklyComponent {
   dias = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'];
 
   eventos = [
-    ['', [
-      'Desenvolvimento Web Full Stack',
-      'Algoritmo e Estrutura de Dados I',
-      'Cálculo I'
-    ]],
-    ['', [
-      'Desenvolvimento Web Full Stack',
-      'Algoritmo e Estrutura de Dados I',
-      'Cálculo I'
-    ]],
-    ['', [
-      'Redes de Computadores'
-    ]],
-    ['', [
-      'Desenvolvimento Web Full Stack',
-      'Inteligência Artificial'
-    ]],
-    ['', [
-      'Desenvolvimento Web Full Stack',
-      'Banco de Dados'
-    ]]
+    [], // Dom
+    ['Desenvolvimento Web Full Stack', 'Algoritmo e Estrutura de Dados I', 'Cálculo I'],
+    ['Desenvolvimento Web Full Stack', 'Algoritmo e Estrutura de Dados I', 'Cálculo I'],
+    ['Redes de Computadores'],
+    ['Desenvolvimento Web Full Stack', 'Inteligência Artificial'],
+    ['Desenvolvimento Web Full Stack', 'Banco de Dados'],
+    [] // Sáb
   ];
 }
