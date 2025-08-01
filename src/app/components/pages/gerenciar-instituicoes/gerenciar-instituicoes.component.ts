63787fd6cb18c7629a853ac6c4515f7d2390abe7
Joa1G <j.gabriel.gomes2976@gmail.com>
2025-07-31 23:49:22 -0400

    feat: funcionalidade de deletar instituicao adicionada

--- src/app/components/pages/gerenciar-instituicoes/gerenciar-instituicoes.component.ts
+++ src/app/components/pages/gerenciar-instituicoes/gerenciar-instituicoes.component.ts
@@ -6,10 +6,12 @@ import { PremiumDialogComponent } from '../../shared/dialogs/premium-dialog/prem
 import { MockedAuthService } from '../../../services/auth/mocked-auth.service';
 import { AbstractInstituicaoService } from '../../../services/instituicao/abstract-instituicao.service';
 import { firstValueFrom } from 'rxjs';
+import { DialogComponent } from "../../shared/dialogs/dialog.component";
+import { AbstractMateriaService } from '../../../services/materia/abstract-materia.service';
 
 @Component({
   selector: 'app-gerenciar-instituicoes',
-  imports: [HeaderComponent, MatIcon, PremiumDialogComponent, RouterModule],
+  imports: [HeaderComponent, MatIcon, PremiumDialogComponent, RouterModule, DialogComponent],
   templateUrl: './gerenciar-instituicoes.component.html',
   styleUrl: './gerenciar-instituicoes.component.scss'
 })
@@ -18,6 +20,15 @@ export class GerenciarInstituicoesComponent {
   private router = inject(Router);
   private authService = inject(MockedAuthService);
   private instituicaoService = inject(AbstractInstituicaoService);
+  private materiasService = inject(AbstractMateriaService);
+
+  showDeleteDialog: boolean = false;
+  selectedInstituicaoId: number | null = null;
+
+  showDeleteMessage(instituicaoId: number) {
+    this.selectedInstituicaoId = instituicaoId;
+    this.showDeleteDialog = true;
+  }
 
   ngOnInit() {
     this.loadUserInstituicoes();
@@ -59,6 +70,32 @@ export class GerenciarInstituicoesComponent {
         console.error('Error loading user institutions:', error);
       }
     }
+  }
+
+  async deleteInstituicao(instituicaoId: number) {
+    const materias = await firstValueFrom(this.materiasService.getMateriasByInstituicaoId(instituicaoId));
+
+    if (materias.data.length > 0) {
+      for (const materia of materias.data) {
+        await firstValueFrom(this.materiasService.deleteMateria(materia.id));
+      }
+    }
+
+    this.instituicaoService.deleteInstituicao(instituicaoId).subscribe({
+      next: (result) => {
+        if (result.success) {
+          console.log('Instituição deletada com sucesso');
+          this.loadUserInstituicoes();
+          this.showDeleteDialog = false;
+          this.selectedInstituicaoId = null;
+        } else {
+          console.error('Erro ao deletar instituição:', result.message);
+        }
+      },
+      error: (error) => {
+        console.error('Erro ao deletar instituição:', error);
+      }
+    });
   }
 
 }
